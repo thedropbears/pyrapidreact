3c645c0fa42dc7b638777cf0b74ad219f2921a33
Lucien Morey <lucienp@live.com.au>
2022-01-19 18:08:41 +1100

    added basic robot.py file

---
+++ robot.py
@@ -0,0 +1,15 @@
+import wpilib
+import ctre
+import magicbot
+
+class MyRobot(magicbot.MagicRobot):
+    flywheelSpeed = magicbot.tunable(0.1)
+
+    def createObjects(self) -> None:
+        self.intakeMotor = ctre.TalonFX(2)
+
+    def teleopPeriodic(self):
+        self.intakeMotor.set(ctre.TalonFXControlMode.PercentOutput, self.flywheelSpeed)
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
