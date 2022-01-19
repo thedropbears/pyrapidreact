4cf4709f9570fc7f8926c4eb698d1a113f5f27f3
David Vo <auscompgeek@users.noreply.github.com>
2022-01-19 18:27:28 +1100

    robot: Run black

--- robot.py
+++ robot.py
@@ -2,6 +2,7 @@ import wpilib
 import ctre
 import magicbot
 
+
 class MyRobot(magicbot.MagicRobot):
     flywheelSpeed = magicbot.tunable(0.1)
 
@@ -10,6 +11,7 @@ class MyRobot(magicbot.MagicRobot):
 
     def teleopPeriodic(self):
         self.intakeMotor.set(ctre.TalonFXControlMode.PercentOutput, self.flywheelSpeed)
+
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
