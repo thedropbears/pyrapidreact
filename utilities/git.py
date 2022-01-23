31594bb9ca203de882a518e3b5936aaf2ca87b2f
David Vo <auscompgeek@users.noreply.github.com>
2022-01-23 11:18:43 +1100

    git: Only write if changed

--- utilities/git.py
+++ utilities/git.py
@@ -6,6 +6,7 @@ def describe() -> str:
     robot_dir = pathlib.Path(__file__).parents[1]
     # would use a hidden file here, but deploy skips dotfiles
     fname = robot_dir / "gitid"
+
     if (robot_dir / ".git").is_dir():
         commit_hash = subprocess.check_output(
             ("git", "describe", "--broken", "--always", "--tags"), text=True
@@ -14,8 +15,18 @@ def describe() -> str:
             ("git", "branch", "--show-current"), text=True
         ).strip()
         desc = f"{commit_hash} ({branch})" if branch else commit_hash
-        with open(fname, "w") as f:
-            f.write(desc)
+
+        try:
+            with open(fname, "r") as f:
+                needs_write = f.read() != desc
+        except FileNotFoundError:
+            needs_write = True
+
+        if needs_write:
+            with open(fname, "w") as f:
+                f.write(desc)
+
         return desc
+
     with open(fname) as f:
         return f.read()
