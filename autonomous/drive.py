35889e1c70c8bafc2a40385deccdc6fadf2717a7
oliverw10 <oliver@warrickfamily.com>
2022-02-05 23:41:05 +1100

    added linear trapazoidal profile generation

--- autonomous/drive.py
+++ autonomous/drive.py
@@ -1,10 +1,15 @@
 from magicbot.state_machine import AutonomousStateMachine, state
-from wpimath import trajectory, geometry, controller, kinematics
+from wpimath import geometry, controller, trajectory
+from wpimath.trajectory import TrapezoidProfile
+from wpimath.geometry import Rotation2d, Translation2d
 
 from components.chassis import Chassis
+from utilities.trajectory_generator import smoothPath
 
 
 class AutoBase(AutonomousStateMachine):
+    """Follows a smoothed linear path between waypoints
+    if it has a ball it stops before the next waypoint to fire it, otherwise dosent stop"""
 
     MODE_NAME = "Drive Backward"
     DEFAULT = True
@@ -13,44 +18,66 @@ class AutoBase(AutonomousStateMachine):
 
     def __init__(self):
         super().__init__()
-        self.max_accel = 0.2
-        self.max_vel = 0.5
-        constraints = trajectory.TrapezoidProfileRadians.Constraints(
-            self.max_vel, self.max_accel
+        # applies to the linear speed, not turning
+        self.linear_constraints = TrapezoidProfile.Constraints(1, 1)
+        # since chassis speeds should be limited in trajectory generation this is high to not hold it back
+        self.drive_rotation_constrants = trajectory.TrapezoidProfileRadians.Constraints(
+            3, 5
         )
         self.drive_controller = controller.HolonomicDriveController(
             controller.PIDController(0, 0, 0),
             controller.PIDController(0, 0, 0),
-            controller.ProfiledPIDControllerRadians(0, 0, 0, constraints),
+            controller.ProfiledPIDControllerRadians(
+                0, 0, 0, self.drive_rotation_constrants
+            ),
         )
-        self.config = trajectory.TrajectoryConfig(self.max_vel, self.max_accel)
-        self.gen = trajectory.TrajectoryGenerator()
-        self.chassis_speeds = kinematics.ChassisSpeeds(0, 0, 0)
 
-    def setup(self):
-        self.config.setKinematics(self.chassis.kinematics)
+        self.waypoints = [
+            geometry.Pose2d(Translation2d(0, 0), Rotation2d(0)),
+            geometry.Pose2d(Translation2d(2, 0), Rotation2d(0)),
+            geometry.Pose2d(Translation2d(4, 2), Rotation2d(0)),
+        ]
+        # both in meters along straight line path
+        self.stop_point = 1
 
-        self.start_position = geometry.Pose2d(
-            geometry.Translation2d(0, 0), geometry.Rotation2d(0)
+        # generates initial
+        self.trap_profile = TrapezoidProfile(
+            self.linear_constraints,
+            goal=TrapezoidProfile.State(self.stop_point, 0),
+            initial=TrapezoidProfile.State(0, 0),
         )
-        self.end_position = geometry.Pose2d(
-            geometry.Translation2d(1, 0), geometry.Rotation2d(0)
-        )
-        self.target_trajectory = self.gen.generateTrajectory(
-            [self.start_position, self.end_position],
-            self.config,
-        )
-        print("total time: ", self.target_trajectory.totalTime())
-        self.next_state("move")
+        self.trap_profile_start_time = 0
 
     @state(first=True)
     def move(self, state_tm):
+        linear_state = self.trap_profile.calculate(
+            state_tm - self.trap_profile_start_time
+        )
+        if False:  # if we want to move stop point
+            self.trap_profile = TrapezoidProfile(
+                self.linear_constraints,
+                goal=TrapezoidProfile.State(self.stop_point, 0),
+                initial=TrapezoidProfile.State(
+                    linear_state.position, linear_state.velocity
+                ),
+            )
+            self.trap_profile_start_time = 0
+            d, vel = self.trap_profile.calculate(
+                state_tm - self.trap_profile_start_time
+            )
+
+        cur_pose = smoothPath(self.waypoints, 0.5, linear_state.position)
         self.chassis_speeds = self.drive_controller.calculate(
             self.chassis.odometry.getPose(),
-            self.target_trajectory.sample(state_tm),
-            self.end_position.rotation(),
+            pose_ref=cur_pose,
+            linearVelocityRef=linear_state.velocity,
+            angleRef=cur_pose.rotation(),  # idk if meant to be angular velocity or current angle or end angle
         )
 
         self.chassis.drive_local(
             self.chassis_speeds.vx, self.chassis_speeds.vy, self.chassis_speeds.omega
         )
+
+    @state
+    def wait(self):
+        pass
