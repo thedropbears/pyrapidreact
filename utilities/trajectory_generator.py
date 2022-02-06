9d761147104c7170dbdb406203fc6e1c594f2ad8
oliverw10 <oliver@warrickfamily.com>
2022-02-06 13:00:14 +1100

    pr comments

--- utilities/trajectory_generator.py
+++ utilities/trajectory_generator.py
@@ -1,4 +1,4 @@
-from wpimath.geometry import Pose2d, Translation2d, Rotation2d
+from wpimath.geometry import Pose2d
 from typing import List
 
 from utilities.scalers import scale_value
@@ -13,35 +13,36 @@ def totalLength(waypoints: List[Pose2d]) -> float:
 
 def lerpPose(p1: Pose2d, p2: Pose2d, n1: float) -> Pose2d:
     n2 = 1 - n1
-    translation = Translation2d(
-        x=p1.translation().x * n2 + p2.translation().x * n1,
-        y=p1.translation().y * n2 + p2.translation().y * n1,
-    )
-    rotation = p1.rotation().radians() * n2 + p2.rotation().radians() * n1
-    return Pose2d(translation, Rotation2d(rotation))
+    translation = p1.translation() * n2 + p2.translation() * n1
+    rotation = p1.rotation() * n2 + p2.rotation() * n1
+    return Pose2d(translation, rotation)
 
 
-def lookupLinear(waypoints: List[Pose2d], d: float) -> Pose2d:
-    """Finds the translation2d d meters along the path of straight line waypoints"""
-    if d < 0:
+def lookupLinear(waypoints: List[Pose2d], dist: float) -> Pose2d:
+    """Finds the translation2d dist meters along the path of straight line waypoints"""
+    if dist < 0:
         return waypoints[0]
     total_dist = 0
     for last, cur in zip(waypoints, waypoints[1:]):
         # distance to next waypoint
         cur_dist = last.translation().distance(cur.translation())
-        if total_dist + cur_dist > d:
-            return lerpPose(last, cur, (d - total_dist) / cur_dist)
+        if total_dist + cur_dist > dist:
+            return lerpPose(last, cur, (dist - total_dist) / cur_dist)
         total_dist += cur_dist
     return waypoints[-1]
 
 
-def smoothPath(waypoints: List[Pose2d], look_around, d, sample_count=10) -> Pose2d:
-    """Samples from a group of waypoints in a way that creates smooth paths"""
-    x, y, o = 0, 0, 0
+def smoothPath(
+    waypoints: List[Pose2d], look_around: float, dist: float, sample_count=10
+) -> Pose2d:
+    """Samples dist meters along waypoints in a way that creates smooth paths"""
+    x, y, angle = 0, 0, 0
     for sample in range(sample_count):
-        sample_d = d + scale_value(sample, 0, sample_count, -look_around, look_around)
+        sample_d = dist + scale_value(
+            sample, 0, sample_count, -look_around, look_around
+        )
         sample_pose = lookupLinear(waypoints, sample_d)
         x += sample_pose.X()
         y += sample_pose.Y()
-        o += sample_pose.rotation().radians()
-    return Pose2d(x / sample_count, y / sample_count, o / sample_count)
+        angle += sample_pose.rotation().radians()
+    return Pose2d(x / sample_count, y / sample_count, angle / sample_count)
