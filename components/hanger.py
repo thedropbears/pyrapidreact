3c35a0fd3cba0aa8dac72ad4a9f9742cc8ddeaba
Lucien Morey <lucienp@Live.com.au>
2022-02-06 17:55:09 +1100

    switched to position control mode

--- components/hanger.py
+++ components/hanger.py
@@ -5,22 +5,34 @@ class Hanger:
     
     climb_motor: rev.CANSparkMax
 
-    climb_speed = tunable(0.5)
+    climb_position = tunable(0.1)
 
-    climb_target_speed = will_reset_to(0.0)
+    climb_target_position = will_reset_to(0.0)
 
-    def __init__(self) -> None:
+    GEAR_RATIO = 15.0
+
+    def setup(self) -> None:
         # check if motor needs to be inverted irl
         self.climb_motor.setInverted(False)
+
+        self.motor_pid = self.climb_motor.getPIDController()
+
+        self.motor_pid.setP(0.1)
+        self.motor_pid.setI(0.0)
+        self.motor_pid.setD(0.0)
+        self.motor_pid.setFF(0.0)
+
+        self.encoder = self.climb_motor.getEncoder()
+        self.encoder.setPositionConversionFactor(self.GEAR_RATIO)
 
     def on_disable(self) -> None:
         self.climb_motor.stopMotor()
 
     def execute(self) -> None:
-        self.climb_motor.set(self.climb_speed)
+        self.motor_pid.setReference(self.climb_target_position, rev.CANSparkMax.ControlType.kPosition)
 
     def winch(self) -> None:
-        self.climb_target_speed = self.climb_speed
+        self.climb_target_position = self.climb_position
 
     def payout(self) -> None:
-        self.climb_target_speed = -self.climb_speed
+        self.climb_target_position = -self.climb_position
