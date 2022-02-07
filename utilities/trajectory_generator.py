654a22a610270f38c94dc07cb199031a17480732
oliverw10 <oliver@warrickfamily.com>
2022-02-07 11:06:54 +1100

    added next_waypoint

--- utilities/trajectory_generator.py
+++ utilities/trajectory_generator.py
@@ -5,6 +5,20 @@ import math
 from utilities.scalers import scale_value
 
 
+def next_waypoint(waypoints: List[Pose2d], dist: float) -> int:
+    """Returns the index of the next waypoint from dist"""
+    if dist < 0:
+        return 0
+    total_dist = 0
+    for last, cur, idx in zip(waypoints, waypoints[1:], range(1, len(waypoints))):
+        # distance to next waypoint
+        cur_dist = last.translation().distance(cur.translation())
+        if total_dist + cur_dist > dist:
+            return idx
+        total_dist += cur_dist
+    return len(waypoints)
+
+
 def total_length(waypoints: List[Pose2d]) -> float:
     return sum(
         last.translation().distance(cur.translation())
