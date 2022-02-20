0f9e0aec085805990645aa90c63ea4b0e33fbcaa
Lucien Morey <lucienp@Live.com.au>
2022-02-20 15:56:22 +1100

    made distance feedback rather than tunable

--- controllers/shooter.py
+++ controllers/shooter.py
@@ -15,7 +15,7 @@ class ShooterController:
     interpolation_override = magicbot.tunable(True)
     flywheel_speed = magicbot.tunable(0.0)
 
-    distance = magicbot.tunable(0.0)
+    distance = 0.0
     ranges_lookup = (2.5, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
     flywheel_speed_lookup = (32.0, 30.0, 36.0, 39.0, 42.0, 46.0, 51.0, 56.0)
 
@@ -30,4 +30,8 @@ class ShooterController:
             self.shooter.motor_speed = interp(
                 self.distance, self.ranges_lookup, self.flywheel_speed_lookup
             )
+
+    @magicbot.feedback
+    def distance_to_goal(self) -> float:
+        return self.distance
         
