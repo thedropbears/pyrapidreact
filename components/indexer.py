d90f4d7d904c9c5564041393b335bf3e42d50408
James Ward <james@thedropbears.org.au>
2022-02-24 16:17:49 +1100

    Refactor indexer
    * Nicer method names
    * More helper methods
    * Split "firing" state from indexer state machine and add to shooter
    * Trivial shooter state machine

--- components/indexer.py
+++ components/indexer.py
@@ -1,56 +1,84 @@
+from turtle import backward
 import rev
 import wpilib
+from enum import Enum
 from magicbot import tunable, feedback
-from typing import Tuple
 
 
 class Indexer:
-    indexer_front_motor: rev.CANSparkMax
-    indexer_back_motor: rev.CANSparkMax
+    class Direction(Enum):
+        OFF = 0
+        FORWARDS = 1
+        BACKWARDS = -1
+
+    # The "tunnel" is the horizontal part of the indexer that the cargo enters first
+    # The "chimney" is the vertical section of the indexer that feeds the shooter
+    # The "cat flap" is the moving flap that allows an opposition cargo to be captured
+    indexer_tunnel_motor: rev.CANSparkMax
+    indexer_chimney_motor: rev.CANSparkMax
     colour_sensor: rev.ColorSensorV3
-    prox_sensor1: wpilib.DigitalInput
-    prox_sensor2: wpilib.DigitalInput
-    # indexer_piston: wpilib.Solenoid
+    tunnel_prox_sensor: wpilib.DigitalInput
+    chimney_prox_sensor: wpilib.DigitalInput
+    cat_flap_piston: wpilib.DoubleSolenoid
 
     is_firing = tunable(False)
     is_red = tunable(False)
     indexer_speed = tunable(0.5)
-    # Front, back
-    speeds = (0.0, 0.0)
+
+    _tunnel_direction = Direction.OFF
+    _chimney_direction = Direction.OFF
+    _cat_flap_is_open = False
+
+    has_trapped_cargo = tunable(False)
 
     def setup(self) -> None:
-        self.indexer_front_motor.setInverted(True)
-        self.indexer_back_motor.setInverted(False)
+        self.indexer_tunnel_motor.setInverted(True)
+        self.indexer_chimney_motor.setInverted(False)
 
     def execute(self) -> None:
-        self.indexer_front_motor.set(self.speeds[0])
-        self.indexer_back_motor.set(self.speeds[1])
+        self.indexer_tunnel_motor.set(self.indexer_speed * self._tunnel_direction)
+        self.indexer_chimney_motor.set(self.indexer_speed * self._chimney_direction)
+        if self._cat_flap_is_open:
+            self.cat_flap_piston.set(wpilib.DoubleSolenoid.Value.kForward)  # TODO check direction
+        else:
+            self.cat_flap_piston.set(wpilib.DoubleSolenoid.Value.kReverse)  # TODO check direction
+
+        # Default state is for nothing to be moving and for the cat flap to be down
+        self._tunnel_direction = self._chimney_direction = Indexer.Direction.OFF
+        self._cat_flap_is_open = False
+
 
     @feedback
-    def has_back(self) -> bool:
-        return not self.prox_sensor1.get() or not self.prox_sensor2.get()
+    def has_cargo_in_chimney(self) -> bool:
+        return not self.tunnel_prox_sensor.get() or not self.chimney_prox_sensor.get()
 
     @feedback
-    def has_front(self) -> bool:
+    def has_cargo_in_tunnel(self) -> bool:
         return self.colour_sensor.getProximity() > 400
 
     @feedback
-    def is_front_ours(self) -> bool:
+    def has_opposition_cargo_in_tunnel(self) -> bool:
         raw = self.colour_sensor.getRawColor()
-        return (raw.red > raw.blue) == self.is_red
+        return self.has_cargo_in_tunnel() and (raw.red > raw.blue) == self.is_red
+
+    @feedback
+    def ready_to_intake(self) -> bool:
+        # We cannot have a cargo in the tunnel, and we can't already have two cargo (one in chimney and one trapped)
+        return not self.has_cargo_in_tunnel() and not (self.has_cargo_in_chimney() and self.has_trapped_cargo)
 
     @feedback
     def get_colours(self) -> str:
         raw = self.colour_sensor.getRawColor()
         return f"r{raw.red:.3f}, g{raw.green:.3f}, b{raw.blue:.3f}"
 
-    @feedback
-    def get_speeds(self) -> Tuple[float, float]:
-        return self.speeds
+    def open_cat_flap(self) -> None:
+        self._cat_flap_is_open = True
 
-    def set(self, front: int, back: int) -> None:
-        self.speeds = (self.indexer_speed * front, self.indexer_speed * back)
+    def close_cat_flap(self) -> None:
+        self._cat_flap_is_open = False
 
-    def set_chute(self, on: bool) -> None:
-        # self.indexer_piston.set(on)
-        pass
+    def run_tunnel_motor(self, direction: Direction) -> None:
+        self._tunnel_direction = direction
+
+    def run_chimney_motor(self, direction: Direction) -> None:
+        self._chimney_direction = direction
