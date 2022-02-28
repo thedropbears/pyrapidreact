7364b1dcabffabcea83e3c6d7dd790e9cbdad326
David Vo <auscompgeek@users.noreply.github.com>
2022-03-01 00:04:18 +1100

    test_scalers: Add more assertions

--- tests/test_scalers.py
+++ tests/test_scalers.py
@@ -10,21 +10,30 @@ from utilities.scalers import apply_deadzone, map_exponential, scale_value
 @given(value=floats(-1, 1), threshold=floats(0, 1, exclude_max=True))
 def test_deadzone(value, threshold):
     result = apply_deadzone(value, threshold)
-    assert abs(result) <= 1
-    if value == 0:
+    if value in (-1, 0, 1):
+        assert result == value
+    elif math.isclose(value, 0, abs_tol=threshold):
         assert result == 0
-    elif not math.isclose(value, 0, abs_tol=threshold):
-        assert math.copysign(1, result) == math.copysign(1, value)
+    elif threshold == 0:
+        assert result == value
+    elif value > 0:
+        assert 0 < result < value
+    else:
+        assert value < result < 0
 
 
-@given(value=floats(-1, 1), base=floats(1, exclude_min=True, allow_infinity=False))
+@given(
+    value=floats(-1, 1),
+    base=floats(1, exclude_min=True, allow_infinity=False, width=16),
+)
 def test_exponential(value, base):
     result = map_exponential(value, base)
-    assert abs(result) <= 1
-    if value == 0:
-        assert result == 0
+    if value in (-1, 0, 1):
+        assert result == value
+    elif value > 0:
+        assert 0 < result < value
     else:
-        assert math.copysign(1, result) == math.copysign(1, value)
+        assert value < result < 0
 
 
 real_halves = floats(allow_nan=False, allow_infinity=False, width=16)
@@ -47,3 +56,7 @@ def test_scale_value(
     assume(min(input_lower, input_upper) <= value <= max(input_lower, input_upper))
     result = scale_value(value, input_lower, input_upper, output_lower, output_upper)
     assert output_lower <= result <= output_upper
+    if value == input_lower:
+        assert result == output_lower
+    elif value == input_upper:
+        assert result == output_upper
