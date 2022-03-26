928981ad3e9630c7682413200230eea331820288
James Ward <james@thedropbears.org.au>
2022-03-26 11:24:09 +1100

    Use proper motor models

--- physics.py
+++ physics.py
@@ -3,9 +3,9 @@ import math
 from pyfrc.physics.core import PhysicsInterface  # type: ignore
 
 import ctre
-import navx
-from utilities.ctre import FALCON_CPR, FALCON_FREE_RPS
+from utilities.ctre import FALCON_CPR, VERSA_ENCODER_CPR
 
+import wpilib.simulation
 from wpimath.kinematics import SwerveDrive4Kinematics, SwerveModuleState
 
 from components.chassis import SwerveModule
@@ -17,64 +17,89 @@ if typing.TYPE_CHECKING:
     from robot import MyRobot
 
 
+class SimpleTalonFXMotorSim:
+    def __init__(self, motor: ctre.TalonFX, kV: float, rev_per_unit: float) -> None:
+        self.sim_collection = motor.getSimCollection()
+        self.kV = kV  # volt seconds per unit
+        self.rev_per_unit = rev_per_unit
+
+    def update(self, dt: float) -> None:
+        voltage = self.sim_collection.getMotorOutputLeadVoltage()
+        velocity = voltage / self.kV  # units per second
+        self.sim_collection.setIntegratedSensorVelocity(
+            int(velocity * self.rev_per_unit * FALCON_CPR / 10)
+        )
+        self.sim_collection.addIntegratedSensorPosition(
+            int(velocity * self.rev_per_unit * FALCON_CPR * dt)
+        )
+
+
+class SimpleTalonSRXMotorSim:
+    def __init__(self, motor: ctre.TalonSRX, kV: float, rev_per_unit: float) -> None:
+        self.sim_collection = motor.getSimCollection()
+        self.kV = kV  # volt seconds per unit
+        self.rev_per_unit = rev_per_unit
+
+    def update(self, dt: float) -> None:
+        voltage = self.sim_collection.getMotorOutputLeadVoltage()
+        velocity = voltage / self.kV  # units per second
+        self.sim_collection.setQuadratureVelocity(
+            int(velocity * self.rev_per_unit * VERSA_ENCODER_CPR / 10)
+        )
+        self.sim_collection.addQuadraturePosition(
+            int(velocity * self.rev_per_unit * VERSA_ENCODER_CPR * dt)
+        )
+
+
 class PhysicsEngine:
     def __init__(self, physics_controller: PhysicsInterface, robot: "MyRobot"):
         self.physics_controller = physics_controller
 
-        self.turret: ctre.WPI_TalonSRX = robot.turret_motor
+        turret_kv = (
+            100000
+            * 1.0
+            / (
+                1023
+                * 12.0
+                * Turret.COUNTS_PER_MOTOR_REV
+                / Turret.COUNTS_PER_TURRET_RADIAN
+                / Turret.pidF
+            )
+        )
+        self.turret = SimpleTalonSRXMotorSim(
+            robot.turret_motor, turret_kv, 1.0 / math.tau
+        )
 
         self.kinematics: SwerveDrive4Kinematics = robot.chassis.kinematics
         self.swerve_modules: list[SwerveModule] = robot.chassis.modules
 
-        self.imu: navx.AHRS = robot.imu
+        # Motors
+        self.wheels = [
+            SimpleTalonFXMotorSim(
+                module.drive, module.drive_ff.kV, 1 / module.DRIVE_MOTOR_REV_TO_METRES
+            )
+            for module in robot.chassis.modules
+        ]
+        self.steer = [
+            SimpleTalonFXMotorSim(
+                module.steer,
+                kV=1,  # TODO: get from sysid logs
+                rev_per_unit=1 / module.STEER_MOTOR_REV_TO_RAD,
+            )
+            for module in robot.chassis.modules
+        ]
+
+        self.imu = wpilib.simulation.SimDeviceSim("navX-Sensor", 4)
+        self.imu_yaw = self.imu.getDouble("Yaw")
 
     def update_sim(self, now: float, tm_diff: float) -> None:
         # turret
-        sign = 1
-        if self.turret.getInverted():
-            sign = -1
-        turret_speed = sign * int(
-            0.5 * self.turret.getMotorOutputPercent() * (1023.0 / Turret.pidF)
-        )
-        self.turret.getSimCollection().setQuadratureVelocity(turret_speed)
-        self.turret.getSimCollection().addQuadraturePosition(
-            int(turret_speed * 10 * tm_diff)
-        )
+        self.turret.update(tm_diff)
 
-        for module in self.swerve_modules:
-            # Drive
-            motor = module.drive
-            drive_max_speed_counts_per_100ms = (
-                SwerveModule.METRES_TO_DRIVE_COUNTS * 4.0 / 10
-            )
-            sign = 1
-            if motor.getInverted():
-                sign = -1
-            velocity = sign * int(
-                drive_max_speed_counts_per_100ms * motor.getMotorOutputPercent()
-            )  # counts per 100ms
-            motor.getSimCollection().setIntegratedSensorVelocity(velocity)
-            motor.getSimCollection().addIntegratedSensorPosition(
-                int(velocity * 10 * tm_diff)
-            )
-
-            # Steer
-            motor = module.steer
-            load_factor = 0.20  # cannot run at free speed
-            sign = 1
-            if motor.getInverted():
-                sign = -1
-            velocity = sign * int(
-                load_factor
-                * FALCON_CPR
-                * motor.getMotorOutputPercent()
-                * FALCON_FREE_RPS
-                / 10
-            )  # counts per 100ms
-            motor.getSimCollection().setIntegratedSensorVelocity(velocity)
-            motor.getSimCollection().addIntegratedSensorPosition(
-                int(velocity * 10 * tm_diff)
-            )
+        for wheel in self.wheels:
+            wheel.update(tm_diff)
+        for steer in self.steer:
+            steer.update(tm_diff)
 
         states = typing.cast(
             typing.Tuple[
@@ -87,8 +112,6 @@ class PhysicsEngine:
         )
         speeds = self.kinematics.toChassisSpeeds(states)
 
-        self.imu.setAngleAdjustment(
-            self.imu.getAngle() - math.degrees(speeds.omega * tm_diff)
-        )
+        self.imu_yaw.set(self.imu_yaw.get() - math.degrees(speeds.omega * tm_diff))
 
         self.physics_controller.drive(speeds, tm_diff)
--- utilities/ctre.py
+++ utilities/ctre.py
@@ -7,6 +7,8 @@ FALCON_CPR = 2048
 # Freespeed in rev/s
 FALCON_FREE_RPS = 100
 
+VERSA_ENCODER_CPR = 4096
+
 
 class TalonEncoder:
     """
