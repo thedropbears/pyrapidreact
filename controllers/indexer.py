91f70f22c437aeda8a04945b6316f2008c1ace69
oliverw10 <oliver@warrickfamily.com>
2022-03-28 10:06:20 +1100

    simplified indexer logic

--- controllers/indexer.py
+++ controllers/indexer.py
@@ -60,31 +60,29 @@ class IndexerController(StateMachine):
     def reading(self, state_tm: float) -> None:
         self.indexer.read_cargo_colour()
         colour = self.indexer.get_cargo_colour()
-        if state_tm > 0.3:
-            if not colour.is_valid() and state_tm < 0.5:
-                return
-            self.log_colour.append(colour.name)
-            if colour.is_opposition() and not self.ignore_colour:
-                if self.catflap_active:
-                    if (
-                        self.indexer.has_trapped_cargo
-                        or self.indexer.has_cargo_in_chimney()
-                    ):
-                        self.next_state("clearing")
-                    else:
-                        self.next_state("trapping")
-                else:
-                    self.next_state("clearing")
+        if state_tm < 0.3 or (not colour.is_valid() and state_tm < 0.5):
+            return
+
+        self.log_colour.append(colour.name)
+        if colour.is_opposition() and not self.ignore_colour:
+            if (
+                self.catflap_active
+                and not self.indexer.has_trapped_cargo
+                and not self.indexer.has_cargo_in_chimney()
+            ):
+                self.next_state("trapping")
             else:
-                if self.catflap_active and self.ignore_colour:
-                    if self.indexer.has_trapped_cargo:
-                        self.next_state("clearing")
-                    else:
-                        self.next_state("trapping")
+                self.next_state("clearing")
+        else:  # our ball or ignore colour
+            if self.catflap_active and self.ignore_colour:
+                if self.indexer.has_trapped_cargo:
+                    self.next_state("clearing")
                 else:
-                    # It is our ball so we have finished this process
-                    # The "stopped" state will work out if it needs to move the ball into the chimney
-                    self.next_state("stopped")
+                    self.next_state("trapping")
+            else:
+                # It is our ball so we have finished this process
+                # The "stopped" state will work out if it needs to move the ball into the chimney
+                self.next_state("stopped")
 
     @state(must_finish=True)
     def clearing(self, initial_call) -> None:
